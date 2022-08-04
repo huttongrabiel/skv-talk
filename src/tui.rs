a46ab00350739192e82eb51af17b0d2464f8f9cf
huttongrabiel <huttonthomas@icloud.com>
2022-08-04 11:07:14 -0700

    Tui: Convert user request to lowercase more permanently
    Instead of checking everytime to_lowercase, just convert to request to
    lowercase.

--- src/tui.rs
+++ src/tui.rs
@@ -8,17 +8,17 @@ pub async fn tui() {
 async fn basic_tui() {
     let mut request_type = String::new();
 
-    while request_type.to_lowercase() != "get"
-        && request_type.to_lowercase() != "put"
-        && request_type.to_lowercase() != "delete"
-        && request_type.to_lowercase() != "ls"
+    while request_type != "get"
+        && request_type != "put"
+        && request_type != "delete"
+        && request_type != "ls"
     {
         println!("Select a request (GET, PUT, DELETE, ls): ");
         io::stdin().read_line(&mut request_type).unwrap();
-        request_type = request_type.trim().to_string();
+        request_type = request_type.trim().to_string().to_lowercase();
     }
 
-    let request_type = match request_type.as_str() {
+    let request_type = match request_type.to_lowercase().as_str() {
         "get" => RequestType::Get,
         "put" => RequestType::Put,
         "delete" => RequestType::Delete,
